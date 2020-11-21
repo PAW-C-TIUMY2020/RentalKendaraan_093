a7b3df7ff45eb81e1e0dd5dc64827248c21c1bb7
Musafak <musafak.ft18@mail.umy.ac.id>
2020-11-21 10:22:23 +0700

    Membuat Fitur Filter

--- RentalKendaraan_093/Controllers/KendaraansController.cs
+++ RentalKendaraan_093/Controllers/KendaraansController.cs
@@ -19,10 +19,30 @@ namespace RentalKendaraan_093.Controllers
         }
 
         // GET: Kendaraans
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string ktsd, string searchString)
         {
-            var rentKendaraanContext = _context.Kendaraan.Include(k => k.IdJenisKendaraanNavigation);
-            return View(await rentKendaraanContext.ToListAsync());
+            //buat list menyimpan ketersediaan
+            var ktsdList = new List<string>();
+            //query mengambil data
+            var ktsdQuery = from d in _context.Kendaraan orderby d.Ketersediaan select d.Ketersediaan;
+
+            ktsdList.AddRange(ktsdQuery.Distinct());
+
+            //untuk menampilkan di view
+            ViewBag.ktsd = new SelectList(ktsdList);
+
+            //panggil db context
+            var menu = from m in _context.Kendaraan.Include(k => k.IdJenisKendaraanNavigation) select m;
+
+            //untuk memilih dropdownlist ketersediaan
+            if(!string.IsNullOrEmpty(ktsd))
+            {
+                menu = menu.Where(x => x.Ketersediaan == ktsd);
+            }
+
+            return View(await menu.ToListAsync());
+            //var rentKendaraanContext = _context.Kendaraan.Include(k => k.IdJenisKendaraanNavigation);
+            //return View(await rentKendaraanContext.ToListAsync());
         }
 
         // GET: Kendaraans/Details/5
